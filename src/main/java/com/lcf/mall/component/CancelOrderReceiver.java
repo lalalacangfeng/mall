e1233b41d046b1475ddfc6f803bc153509e3ac6e
lcf <546378592@qq.com>
2020-10-13 22:12:49 +0800

    整合RabbitMQ实现延迟消息

---
+++ src/main/java/com/lcf/mall/component/CancelOrderReceiver.java
@@ -0,0 +1,28 @@
+package com.lcf.mall.component;
+
+import com.lcf.mall.service.OmsPortalOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 取消订单消息的处理者
+ */
+@Component
+@RabbitListener(queues = "mall.order.cancel")
+public class CancelOrderReceiver {
+
+    private static Logger logger = LoggerFactory.getLogger(CancelOrderReceiver.class);
+
+    @Autowired
+    private OmsPortalOrderService portalOrderService;
+
+    @RabbitHandler
+    public void handle(Long orderId){
+        logger.info("receive delay message orderId:{}",orderId);
+        portalOrderService.cancelOrder(orderId);
+    }
+}
